e72b1990ba1551f72b7b21e934a9ab320f6bc30a
Cindy Lam <cindybinh444@gmail.com>
2021-08-20 20:34:06 -0400

    Moved server related files into the server folder and added sendFile codes in server

--- server/server.js
+++ server/server.js
@@ -6,6 +6,13 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+if(process.env.NODE_ENV === 'production'){
+  //set static folder
+  app.use(express.static('client/build'));
+}
+app.get('*',(req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 
 // API ENDPOINT
 app.get("/api", (req, res) => {
