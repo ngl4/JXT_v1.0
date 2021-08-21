c3fea5e12c26f134311f0bf3082d767f4245376a
Cindy Lam <cindybinh444@gmail.com>
2021-08-20 22:07:39 -0400

    created another build and re-added path again
    maybe it was the old build that caused the issue

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 // server/index.js
-// const path = require("path");
+const path = require("path");
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'build')));
 
-// app.get('/*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
-// });
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 // API ENDPOINT
 app.get("/api", (req, res) => {
