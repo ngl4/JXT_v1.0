d1c332713c7e8fcaa47570c30f9e4e2ee404eb34
Cindy Lam <cindybinh444@gmail.com>
2021-08-20 21:05:45 -0400

    Updated package-lock.json and removed extra comments in server.js

--- server/server.js
+++ server/server.js
@@ -1,11 +1,8 @@
 // server/index.js
-// const path = require('path');
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// const buildPath = path.join(__dirname, '..', 'build');
-// app.use(express.static(buildPath));
 
 // API ENDPOINT
 app.get("/api", (req, res) => {
