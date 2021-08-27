f8c72f9a223f71a67a4509fd2b3fd4fcf150293e
Cindy Lam <cindybinh444@gmail.com>
2021-08-26 23:22:17 -0400

    Installed mongoose and added logic to server.js

--- server.js
+++ server.js
@@ -2,9 +2,23 @@
 const path = require("path");
 const express = require("express");
 const app = express();
+const mongoose = require('mongoose');
 const PORT = process.env.PORT || 3001;
 
 app.use(express.static(path.join(__dirname, 'build')));
+
+mongoose.connect('mongodb://localhost:27017/testDB', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const User = mongoose.model('User', { _id: Number, name: String, age: Number });
+
+const user = new User ({ //create a new user specific data 
+  _id: 2,
+  name: "Victor",
+  age: 59
+});
+
+user.save(); //TODO: THIS IS NOT WORKING YET AND THE TESTDB USER MODEL IS NOT UPDATED?? 
+//-- maybe consider inserting a new user instead of creating a new user 
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
