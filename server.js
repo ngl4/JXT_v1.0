66c7006a0008dd87d7a699ab139caad3a3a47208
Cindy Lam <cindybinh444@gmail.com>
2021-09-04 23:26:49 -0400

    Completed testing - able to retrieve data from MongoDB to server.js and then send to the frontend to display in console.log

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ app.use(require("body-parser").json());
 
 mongoose.connect('mongodb://localhost:27017/jxtrackDB', {useNewUrlParser: true, useUnifiedTopology: true});
 
+const userSchema = new mongoose.Schema ({
+  username: String, 
+  password: String
+}); 
+
+const User = mongoose.model('User', userSchema);
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
@@ -20,27 +26,35 @@ app.get("/api", (req, res) => {
     res.json({ message: "JXT copyright 2021" });
   });
 
-// Test - post to the database 
+// Test - SEND DATA TO DB - post to the database 
 app.post("/create", (req, res) => {
   console.log("here");
   console.log(req.body); 
- 
-  const userSchema = new mongoose.Schema ({
-    username: String, 
-    password: String
-  }); 
-
-  const User = mongoose.model('User', userSchema);
 
   const user = new User ({
     username: req.body.username,
     password: req.body.password
   });
   
-  user.save(); //TODO: Issue why is it not saving in the User folder, but inside Collections folder?? 
+  user.save(); 
 
   res.json({message: "Saved to DB"});
 
+});
+
+//Test - RETRIEVE DATA FROM DB - check if the user exist and then send info from backend storage to frontend display
+app.get("/users", (req, res) => {
+  //access the users model to retrieve information regarding All Users - username and password 
+  User.find({}, (err, foundAllUsers) => {
+    if (!err) {
+      res.send({
+        allUsers: foundAllUsers,
+        message: "success"
+      });
+    }else {
+      res.send(err); 
+    }
+  });
 });
 
 
