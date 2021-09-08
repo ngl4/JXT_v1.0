c21f5c1572d0bbca72f347f9839c97a6a8018e26
Cindy Lam <cindybinh444@gmail.com>
2021-09-07 23:05:21 -0400

    Updated the insert page and completed the TODO to set up checkboxes

--- client/src/components/DataInsert/InsertPage.jsx
+++ client/src/components/DataInsert/InsertPage.jsx
@@ -4,6 +4,16 @@ import Input from "../UI/input";
 function InsertPage() {
     const [companyName, setCompanyName] = useState("");
     const [jobURL, setJobURL] = useState("");
+    const [checkNew, setCheckNew] = useState(false);
+    const [applyDate, setApplyDate] = useState("");
+    const [checkApplied, setCheckApplied] = useState(false);
+    const [appliedDate, setAppliedDate] = useState("");
+    const [checkPhoneCalled, setCheckPhoneCalled] = useState(false);
+    const [phoneCallDate, setPhoneCallDate] = useState("");
+    const [checkInterviewed, setCheckInterviewed] = useState(false);
+    const [interviewDate, setInterviewDate] = useState("");
+    // const [enabled, setEnabled] = useState("");  //TODO: Set up a disabled logic when a checkbox is chosen, the other checkboxes will be disabled 
+    // const [disabled, setDisabled] = setState(false);
 
     function handleChange(event) {
         const {value, name} = event.target; 
@@ -14,7 +24,47 @@ function InsertPage() {
             break;
             case 'jobURL':
                 setJobURL(value);
-            break;        
+            break;   
+            case 'new':
+                if (checkNew) {
+                    setCheckNew(false);
+                }else {
+                    setCheckNew(true);  
+                }
+            break;  
+            case 'applied':
+                if (checkApplied) {
+                    setCheckApplied(false);
+                }else {
+                    setCheckApplied(true);  
+                }
+            break; 
+            case 'phoneCalled':
+                if (checkPhoneCalled) {
+                    setCheckPhoneCalled(false);
+                }else {
+                    setCheckPhoneCalled(true);  
+                }
+            break; 
+            case 'interviewed':
+                if (checkInterviewed) {
+                    setCheckInterviewed(false);
+                }else {
+                    setCheckInterviewed(true);  
+                }
+            break;                             
+            case 'applyDate':
+                setApplyDate(value);
+            break;  
+            case 'appliedDate':
+                setAppliedDate(value);
+            break; 
+            case 'phoneCallDate':
+                setPhoneCallDate(value);
+            break;      
+            case 'interviewDate':
+                setInterviewDate(value);
+            break;                                   
             default:
                 console.log("Unidentified input field found in the insertPage form");
         }
@@ -37,6 +87,7 @@ function InsertPage() {
     return (
         <div>
             <form onSubmit={handleSubmit}>
+            {/* Input Company Name */}
             <div className = "row mb-3 d-flex justify-content-center">
                     <label className="col-sm-2 col-form-label">Company Name</label>
                     <div className="col-sm-2">
@@ -49,6 +100,7 @@ function InsertPage() {
                         /> 
                     </div>
                 </div>
+                {/* Input Job Post URL */}
                 <div className = "row mb-3 d-flex justify-content-center">
                     <label className="col-sm-2 col-form-label">Job Post URL</label>
                     <div className="col-sm-2">
@@ -61,7 +113,88 @@ function InsertPage() {
                     />   
                     </div>
                 </div>
-                {/* TODO : Checkbox - New, Applied, Phone-Called, Interviewed */}
+                {/*Checkbox - New, Applied, Phone-Called, Interviewed */}
+                <div className = "column mt-4 mb-4 d-flex justify-content-center">
+                    <div className="form-check form-check-inline">
+                        <input className="form-check-input" type="checkbox" id="inlineCheckbox1" name="new" value="new" onChange = {handleChange} />
+                        <label className="form-check-label" for="inlineCheckbox1">New</label>
+                    </div>
+                    <div className="form-check form-check-inline">
+                        <input className="form-check-input" type="checkbox" id="inlineCheckbox2" name="applied" value="applied" onChange = {handleChange} />
+                        <label className="form-check-label" for="inlineCheckbox2">Applied</label>
+                    </div>
+                    <div className="form-check form-check-inline">
+                        <input className="form-check-input" type="checkbox" id="inlineCheckbox3" name="phoneCalled" value="phoneCalled" onChange = {handleChange} />
+                        <label className="form-check-label" for="inlineCheckbox3">Phone-Called</label>
+                    </div>
+                    <div className="form-check form-check-inline">
+                        <input className="form-check-input" type="checkbox" id="inlineCheckbox4" name="interviewed" value="interviewed" onChange = {handleChange} />
+                        <label className="form-check-label" for="inlineCheckbox4">Interviewed</label>
+                    </div>   
+                </div> 
+                <div className="d-flex justify-content-center mt-3 mb-5">
+                {/* Checkbox Specific - New */}
+                    {checkNew ?                 
+                    <div className = "row mb-3 d-flex justify-content-center">
+                        <label className="col-sm-5 col-form-label">Date to Apply:</label>
+                        <div className="col-sm-5">
+                            <Input 
+                                name = "applyDate"
+                                type = "date"
+                                onChange = {handleChange}
+                                placeholder = "Enter Date"
+                                value = {applyDate}
+                            />   
+                        </div>
+                    </div> 
+                    : null}
+                {/* Checkbox Specific - Applied */}
+                    {checkApplied ?                 
+                        <div className = "row mb-3 d-flex justify-content-center">
+                            <label className="col-sm-5 col-form-label">Applied Date:</label>
+                            <div className="col-sm-5">
+                                <Input 
+                                    name = "appliedDate"
+                                    type = "date"
+                                    onChange = {handleChange}
+                                    placeholder = "Enter Date"
+                                    value = {appliedDate}
+                                />   
+                            </div>
+                        </div> 
+                    : null}                
+                {/* Checkbox Specific - Phone Called */}
+                    {checkPhoneCalled ?                 
+                            <div className = "row mb-3 d-flex justify-content-center">
+                                <label className="col-sm-6 col-form-label">Phone Call(ed) Date:</label>
+                                <div className="col-sm-6">
+                                    <Input 
+                                        name = "phoneCallDate"
+                                        type = "date"
+                                        onChange = {handleChange}
+                                        placeholder = "Enter Date"
+                                        value = {phoneCallDate}
+                                    />   
+                                </div>
+                            </div> 
+                    : null}                    
+                {/* Checkbox Specific - Interviewed */}    
+                    {checkInterviewed ?                 
+                            <div className = "row mb-3 d-flex justify-content-center">
+                                <label className="col-sm-6 col-form-label">Interview(ed) Date:</label>
+                                <div className="col-sm-6">
+                                    <Input 
+                                        name = "interviewDate"
+                                        type = "date"
+                                        onChange = {handleChange}
+                                        placeholder = "Enter Date"
+                                        value = {interviewDate}
+                                    />   
+                                </div>
+                            </div> 
+                    : null}                    
+                </div>
+                {/* Submit Button - Add Job to Database */}
                 <div className="d-flex justify-content-center mt-3 mb-5">
                     <Input
                         type = "submit"
