fb7bded7d024ea95e8807a7366a5da45d8304ac6
Cindy Lam <cindybinh444@gmail.com>
2021-09-09 21:48:21 -0400

    Added TrackPage and Updated App.js with React-Router-Dom npm

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,14 @@
 import React from "react";
 import './App.css';
 import InsertPage from "./components/DataInsert/InsertPage";
+import TrackPage from "./components/Dashboard/TrackPage";
+import Navbar from "./components/UI/navbar";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   const [data, setData] = React.useState(null);
@@ -9,18 +17,43 @@ function App() {
     fetch("/api")
       .then((res) => res.json())  
       .then((data) => setData(data.message));  
-
   });
+
+  const linkStyle = {
+    margin: "0rem",
+    textDecoration: "none",
+    color: 'white'
+  };
 
   return (
     <div className="container-fluid">
+      
       <header className="row m-5">
-        <h1 className="text-center">Welcome to JXT Job Track App</h1>
+      <Router>
+        <div>
+          <Navbar
+            RouterLinkHome = {<Link to="/" style={linkStyle}>JXT Job Tracker</Link>}
+            RouterLinkEnter = {<Link to="/enter-page" style={linkStyle}>Enter</Link>}
+            RouterLinkTrack = {<Link to="/track-page" style={linkStyle}>Track</Link>}
+          />
+        <Switch>
+        <body className="row d-flex justify-content-center align-items-center mt-5">
+          <Route exact path="/">
+            <h1 className="text-center">Welcome to JXT Job Track App!</h1>
+          </Route>
+          <Route path="/enter-page">
+            <InsertPage />
+          </Route>
+          <Route path="/track-page">
+            <TrackPage />
+          </Route>
+          </body>
+        </Switch>
+        </div>
+      </Router>
+        {/* <h1 className="text-center">Welcome to JXT Job Track App</h1> */}
       </header>
-      <body className="row d-flex justify-content-center align-items-center">
-      {<InsertPage />}
-      </body>
-      <footer>
+      <footer className="fixed-bottom">
         <p className="cpText text-center mt-5">{ !data ? "Loading..." : data }</p>
       </footer>
     </div>
---
+++ client/src/components/Dashboard/TrackPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function TrackPage() {
+    return (
+        <div className="text-center">
+            YAY! THIS IS THE TRACK PAGE!!! 
+        </div>
+    );
+};
+
+export default TrackPage;
