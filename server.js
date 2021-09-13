adb697838bd2f0afa53acd8b24fa2305def58d22
Cindy Lam <cindybinh444@gmail.com>
2021-09-12 20:54:42 -0400

    fixed bug and updated the connection string to uri

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(require("body-parser").json());
 //console.log(`mongodb+srv://admin-cindy:${process.env.DB_MONGOSH_PW}@clustertestjxt.wthnh.mongodb.net/jobAppsDB`);
 
 //LIVE AWS CLOUD STORAGE - mongoosh + mongoAtlas 
-mongoose.connect(`mongodb+srv://admin-cindy:${uri}@clustertestjxt.wthnh.mongodb.net/jxtrackDB`, {useNewUrlParser: true, useUnifiedTopology: true}); 
+mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true}); 
 
 // LOCAL STORAGE
 //mongoose.connect('mongodb://localhost:27017/jxtrackDB', {useNewUrlParser: true, useUnifiedTopology: true}) 
