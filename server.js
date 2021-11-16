14bd457c3905bb2a9544620453046cff75977699
Cindy Lam <cindybinh444@gmail.com>
2021-11-15 21:29:35 -0500

    (DEBUG ATTEMPT) Added render to secret route

--- server.js
+++ server.js
@@ -117,7 +117,8 @@ app.get("/auth/google/jxt",
 app.get("/secret", function(req, res){
   console.log(req.isAuthenticated());
   if (req.isAuthenticated()){
-    res.json({ message: "true" })
+    res.json({ message: "true" });
+    res.render("/secret");
   }else {
     res.json({ message: "false" })
     res.render("/");
