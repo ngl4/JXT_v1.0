e6c8554c1014aa12f36dbee5fb91135f855c5794
Cindy Lam <cindybinh444@gmail.com>
2021-11-16 23:29:31 -0500

    (DEBUG ATTEMPT) revert previous changes back

--- server.js
+++ server.js
@@ -107,7 +107,7 @@ passport.use(new GoogleStrategy({
 app.get("/auth/google", 
   passport.authenticate("google", { scope : ["profile", "email"] })); 
 
-app.get("https://jxt-app-v1.herokuapp.com/auth/google/jxt", 
+app.get("/auth/google/jxt", 
   passport.authenticate("google", { failureRedirect: "/" }),
   function(req, res) {
     // Successful authentication, redirect jxt.
