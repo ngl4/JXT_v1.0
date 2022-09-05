18fff733f948961832778518156eb23485006fdd
Cindy Lam <cindybinh444@gmail.com>
2022-09-05 13:02:26 -0400

    (LEARNING) write simple tests

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,15 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+it("renders the banner and links", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByRole("banner")).toBeInTheDocument();
+  expect(screen.getByRole("link", { name: "Enter" })).toBeInTheDocument();
+  expect(screen.getByRole("link", { name: "Track" })).toBeInTheDocument();
 });
--- client/src/components/__tests__/app.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen, cleanup } from "@testing-library/react";
-import App from "../../App";
-
-test("renders the landing page", () => {
-  render(<App />);
-  expect(screen.getByRole("banner")).toBeInTheDocument();
-  expect(screen.getByRole("link", { name: "Enter" })).toBeInTheDocument();
-});
