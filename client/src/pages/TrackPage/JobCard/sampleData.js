bbc6b5634e565475c92759864eb305c468e59acd
Cindy Lam <cindybinh444@gmail.com>
2022-09-05 20:44:32 -0400

    (DEV) created a sample data for temp view

---
+++ client/src/pages/TrackPage/JobCard/sampleData.js
@@ -0,0 +1,72 @@
+export const SampleData = [
+  {
+    jobTitle: "Software Engineer",
+    companyName: "XCompany",
+    savedNotes: [
+      {
+        date: "08-23-22",
+        note: "This is just a sample company",
+      },
+    ],
+    lastUpdated: "3", //only for mocked generally. In reality, we should be seeing a datetimestamp
+  },
+  {
+    jobTitle: "Frontend Software Engineer",
+    companyName: "YCompany",
+    savedNotes: [
+      {
+        date: "08-25-22",
+        note: "This is just a sample company",
+      },
+    ],
+    lastUpdated: "5",
+  },
+  {
+    jobTitle: "Fullstack Software Engineer",
+    companyName: "ZCompany",
+    savedNotes: [
+      {
+        date: "08-27-22",
+        note: "This is just a sample company",
+      },
+    ],
+    lastUpdated: "15",
+  },
+  {
+    jobTitle: "Backend Software Engineer",
+    companyName: "OCompany",
+    savedNotes: [
+      {
+        date: "08-29-22",
+        note: "This is just a sample company",
+      },
+    ],
+    lastUpdated: "30",
+  },
+  {
+    jobTitle: "Operations Software Engineer",
+    companyName: "PCompany",
+    savedNotes: [
+      {
+        date: "08-29-22",
+        note: "This is just a sample company",
+      },
+    ],
+    lastUpdated: "50",
+  },
+  {
+    jobTitle: "QA Software Engineer",
+    companyName: "QCompany",
+    savedNotes: [
+      {
+        date: "08-30-22",
+        note: "This is just a sample company",
+      },
+      {
+        date: "08-31-22",
+        note: "uhm just check on this company!",
+      },
+    ],
+    lastUpdated: "55",
+  },
+];
